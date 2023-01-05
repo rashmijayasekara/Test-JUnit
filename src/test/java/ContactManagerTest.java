ea82ba54100b1a9643a4a0a34abc90e79f0d73f2
Rashmi <rashmijayasekara00@gmail.com>
2023-01-05 16:55:35 +0530

    Adding Conditional Executions

--- src/test/java/ContactManagerTest.java
+++ src/test/java/ContactManagerTest.java
@@ -1,13 +1,14 @@
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 
-
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)  // so JUnit will create only one instance of the class
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)  // so JUnit will create only one instance of the class
 class ContactManagerTest {
     ContactManager contactManager;
 
     @BeforeAll
-    public void setUpAll(){
+    public static void setUpAll(){
         System.out.println("Should print before all tests. This method is static because creating the instance otherwise Junit can't execute this method");
         System.out.println("With the implementation of the TestInstance this test class will only instantiate once there for this test method do not need to be a static method");
        }
@@ -78,6 +79,16 @@ class ContactManagerTest {
             contactManager.addContact("Rashmi","Nishani","077defghijkl");
         });
     }
+    @EnabledOnOs(value = OS.LINUX,disabledReason = "Enabled on Linux OS")
+    @Test
+    @DisplayName("Should not create contact when the first name is null Linux")  // provides a customized name to the test method
+    public void shouldThrowRuntimeExceptionWhenFirstNameIsNullLinux(){
+        ContactManager contactManager = new ContactManager();
+        Assertions.assertThrows(RuntimeException.class,()->{
+            contactManager.addContact(null,"Jayasekara","0773376993");
+        });
+    }
+
 
     @AfterEach
     public void tearDown(){
@@ -85,9 +96,11 @@ class ContactManagerTest {
     }
 
     @AfterAll
-    public void tearDownAll(){
+    public static void tearDownAll(){
         System.out.println("Should execute after all tests");
         System.out.println("Previously this test method was a static method since it executes after all, but with the implementation of the annotation TestInstance no need this to be a static method");
     }
+
+
 
 }
