caabeb46b2edaa709930e4d1e96e9016879e2d31
Rashmi <rashmijayasekara00@gmail.com>
2023-01-05 15:15:56 +0530

    Test getAllContacts()

--- src/test/java/ContactManagerTest.java
+++ src/test/java/ContactManagerTest.java
@@ -1,5 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ContactManagerTest {
+
+    @Test
+    public void shouldCreateContact(){
+        ContactManager contactManager = new ContactManager();
+        contactManager.addContact("John","Doe","0773376993");
+        Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
+        Assertions.assertEquals(1,contactManager.getAllContacts().size());
+    }
 
 }
